2e1ab94f244807baf9271a4aacaa6db7bcc4af93
mBuergi86 <markus.buergi@edu.teko.ch>
2023-06-20 21:56:36 +0200

    Sample of lecture

---
+++ muster/todo-backend/models/persistence.go
@@ -0,0 +1,9 @@
+// Package models contains the todo backend models and corresponding business logic
+package models
+
+type TodoPersistence interface {
+	ReadTodos() ([]Todo, error)
+	ReadTodoById(string) (Todo, error)
+	CreateTodo(Todo) (Todo, error)
+	UpdateTodo(string, Todo) (Todo, error)
+}
