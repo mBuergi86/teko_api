2bea3e01c1fc5711d90ffc5f1ddbc01263f1a436
Markus Buergi <m.buergi@signx.ch>
2023-06-25 10:45:03 +0200

    Fix

--- middlewares/middleware.go
+++ middlewares/middleware.go
@@ -1,20 +1,12 @@
 package middlewares
 
 import (
-	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	recover2 "github.com/gofiber/fiber/v2/middleware/recover"
 )
 
-func Middleware(app *fiber.App) fiber.Handler {
+func Middleware(app *fiber.App) {
 	app.Use(logger.New())
 	app.Use(recover2.New())
-
-	return func(ctx *fiber.Ctx) error {
-
-		fmt.Printf("INFO: %s %s\n", ctx.Method(), ctx.Path())
-
-		return ctx.Next()
-	}
 }
