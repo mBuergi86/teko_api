a361a0fc5ebca5fb9fad965ee715ee09378c511e
Markus Buergi <m.buergi@signx.ch>
2023-06-25 10:42:38 +0200

    Fix

--- server.go
+++ server.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"awesomeProject/middleware"
+	"awesomeProject/middlewares"
 	"awesomeProject/router"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New()
-	middleware.Middleware(app)
+	middlewares.Middleware(app)
 	router.Routing(app)
 
 	err := app.Listen(":8080")
